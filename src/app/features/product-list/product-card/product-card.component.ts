00b6cc185928375d547260e06a22ce20a672f4b6
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2025-04-22 18:00:38 +0200

    card styled

---
+++ src/app/features/product-list/product-card/product-card.component.ts
@@ -0,0 +1,42 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../../shared/models/products.model';
+import { PrimaryButtonComponent } from '../../../shared/primary-button/primary-button.component';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [PrimaryButtonComponent],
+  template: `
+    <section
+      class="bg-white shadow-md rounded-xl p-6 flex flex-col gap-6 relative"
+    >
+      <div class="mx-auto">
+        <img
+          [src]="product().image"
+          alt=""
+          class="w-[200px] h-[100px] object-contain"
+        />
+      </div>
+      <div class="flex flex-col mt-2">
+        <span class="font-bold text-md">{{ product().title }}</span>
+        <span class="text-sm">{{ '€' + product().price }}</span>
+        <app-primary-button 
+            class="mt-3"
+            label="Add to cart" 
+            (btnClicked)="onAddToCart()" />
+      </div>
+        <span class="absolute top-2 right-3 text-sm font-bold"
+              [class]="product().stock ? 'text-green-500' : 'text-red-500'">
+        @if (product().stock) { 
+          {{ product().stock }} left } @else { Out of stock}
+        </span>
+    </section>
+  `,
+  styles: ``,
+})
+export class ProductCardComponent {
+  product = input.required<Product>();
+
+  onAddToCart() {
+    console.log('Add to cart clicked!', this.product());
+  }
+}
