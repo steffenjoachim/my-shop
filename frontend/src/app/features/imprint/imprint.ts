91252440d7f3203dc27f1b3d58de856e79bf6395
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2025-10-21 15:42:45 +0200

    footer links preparation

---
+++ frontend/src/app/features/imprint/imprint.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imprint',
+  imports: [],
+  template: `
+    <p>
+      imprint works!
+    </p>
+  `,
+  styles: ``
+})
+export class Imprint {
+
+}
