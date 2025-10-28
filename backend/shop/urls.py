f1f1e0a165a5af662f9e3a960cb22232f91e66f3
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2025-10-28 18:21:12 +0100

    img url is saved correctly

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -11,7 +11,6 @@ from .views import (
     get_csrf_token,
     ReviewViewSet,
     OrderViewSet,
-    OrderListCreateView,
 )
 
 router = DefaultRouter()
@@ -42,6 +41,4 @@ urlpatterns = [
 
     # CSRF-Token für Frontend
     path("cart/csrf/", get_csrf_token, name="get-csrf-token"),
-    path("orders/", OrderListCreateView.as_view(), name="order-list-create"),
-
 ]
